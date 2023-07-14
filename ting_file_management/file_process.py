07c1aa4eb6fd87a03612b215bdcf615659ef1dfd
Gzuuu <jesue_gege@hotmail.com>
2023-07-14 16:25:59 -0300

    requisito 3 concluido

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    
+    conteudo = txt_importer(path_file)
+    conteudo_para_fila = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(conteudo),
+        "linhas_do_arquivo": conteudo,
+    }
+    instance.enqueue(conteudo_para_fila)
+    sys.stdout.write(f"{conteudo_para_fila}\n")
+    return conteudo_para_fila
 
 
 def remove(instance):
