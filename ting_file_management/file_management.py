84d180d8187bb518c0144e54336bc476b3106bd2
Gzuuu <jesue_gege@hotmail.com>
2023-07-14 15:48:44 -0300

    requisito 2 concluido

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write('Formato inválido')
+        with open(path_file, 'r') as arquivo:
+            conteudo = arquivo.read()
+        quebra_de_linha = conteudo.splitlines()
+        return quebra_de_linha
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    
