f4873f4f343a145a1a881f11724ce56a2a1e4282
Gzuuu <jesue_gege@hotmail.com>
2023-07-17 14:51:40 -0300

    requisito 8 concluido

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,3 +20,23 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    archives = []
+    for index, file in enumerate(instance._data):
+        archive_info = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for line, line_content in enumerate(
+            file["linhas_do_arquivo"], start=1
+        ):
+            if word.lower() in line_content.lower():
+                archive_info["ocorrencias"].append(
+                    {
+                        "linha": line,
+                        "conteudo": line_content
+                    })
+        if archive_info["ocorrencias"]:
+            archives.append(archive_info)
+
+    return archives
